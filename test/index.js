15181a7bc5b9de2474ded34dc8f6aa8f5b5d82dd
Salim <salimhbalfas@gmail.com>
2017-02-09 01:27:34 +0700

    Print name only

--- test/index.js
+++ test/index.js
@@ -15,7 +15,7 @@ p2p.ignore(function (infohash, rinfo, callback) {
 });
 
 p2p.on('metadata', function (metadata) {
-    console.log(metadata);
+    console.log(metadata.info.Dict.name);
 });
 
 p2p.listen(6881, '0.0.0.0');
