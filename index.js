81948e0b045bc4537f082a2c9fbe590352a3d5a6
Salim <salimhbalfas@gmail.com>
2017-02-09 02:36:45 +0700

    fix :

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Torrent = require('./schemes/Torrent.js');
+
+mongoose.connect('mongodb://digital_ocean:digital123@ds145158.mlab.com:45158/metadata_test');
+mongoose.Promise = Promise;
+
+var P2PSpider = require('./lib');
+
+var p2p = P2PSpider({
+    nodesMaxSize: 200,   // be careful
+    maxConnections: 400, // be careful
+    timeout: 5000
+});
+
+p2p.ignore(function (infohash, rinfo, callback) {
+    // false => always to download the metadata even though the metadata is exists.
+    var theInfohashIsExistsInDatabase = false;
+    callback(theInfohashIsExistsInDatabase);
+});
+
+p2p.on('metadata', function (metadata) {
+    console.log(metadata);
+
+    // new Torrent({
+    //   '_id': metadata.infoHash,
+    //   'title': metadata.name,
+    //   'details': metadata.announce,
+    //   'size': metadata.length,
+    //   'files': metadata.files.map(f => f.path),
+    //   'imported': new Date()
+    // }).save()
+
+});
+
+p2p.listen(6881, '0.0.0.0');
