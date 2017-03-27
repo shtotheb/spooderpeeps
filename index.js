4484e18cef784975052a40f08cdce29d0d542b8e
Salim <salimhbalfas@gmail.com>
2017-03-28 00:57:23 +0700

    Minor fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ p2p.on('metadata', function (metadata) {
 	var record = {};
   var exists = false;
   Records.findById(metadata.infohash).exec(exists = true);
-  console.log("Metadata found!!  ", metadata.info.name, " exists = ", exists);
+  console.log("Metadata found!!  ", metadata.info.name.toString(), " exists = ", exists);
 	if(typeof metadata.info.name !== 'undefined' && typeof metadata.info.files !== 'undefined' && exists == false){
 		record.name = metadata.info.name.toString();
 		record.search = record.name.replace(/\.|\_/g, ' ');
