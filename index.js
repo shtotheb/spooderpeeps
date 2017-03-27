9184023806921fdae707dd0f29a30856005a3642
Salim <salimhbalfas@gmail.com>
2017-03-28 00:13:22 +0700

    Digital ocean test 9

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ p2p.ignore(function (infohash, rinfo, callback) {
 p2p.on('metadata', function (metadata) {
 	var record = {};
   console.log("Metadata found!!");
-  console.log(Records.findById(metadata.infohash).exec() == null);
+  console.log(Records.findById(metadata.infohash).exec() == false);
 	if(typeof metadata.info.name !== 'undefined' && typeof metadata.info.files !== 'undefined' && Records.findById(metadata.infohash).exec() == null){
 		record.name = metadata.info.name.toString();
 		record.search = record.name.replace(/\.|\_/g, ' ');
