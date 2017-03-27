5fad70269e52ed243c641e71a4531e1a2ac52478
Salim <salimhbalfas@gmail.com>
2017-03-28 00:26:46 +0700

    Digital ocean test 12

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ p2p.ignore(function (infohash, rinfo, callback) {
 
 p2p.on('metadata', function (metadata) {
 	var record = {};
-  console.log("Metadata found!!");
-  console.log(Records.findById(metadata.infohash) == null);
+  var exists = true;
+  Records.findById(metadata.infohash).exec(exists = false);
+  console.log("Metadata found!! exists = ", exists);
 	if(typeof metadata.info.name !== 'undefined' && typeof metadata.info.files !== 'undefined' && Records.findById(metadata.infohash) == null){
 		record.name = metadata.info.name.toString();
 		record.search = record.name.replace(/\.|\_/g, ' ');
