7de7fc19552d24a938a7d24b7082e4b2aee7afef
Salim <salimhbalfas@gmail.com>
2017-03-28 14:19:57 +0700

    Minor fix

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ p2p.on('metadata', function (metadata) {
 
 	if(typeof metadata.info.name !== 'undefined' && typeof metadata.info.files !== 'undefined'){
 
-    record.size = 0;
+    var record = 0;
 
 		if(typeof metadata.info.files !== 'undefined' && metadata.info.files.length < 100){
 			var record = [];
 			metadata.info.files.forEach(function(element){
-          record.size = record.size + element.length;
+          record = record + element.length;
 			});
 		}
 
@@ -53,7 +53,7 @@ p2p.on('metadata', function (metadata) {
       'name': metadata.info.name.toString(),
       'search': metadata.info.name.replace(/\.|\_/g, ' '),
       'magnet': metadata.magnet,
-      'size': record.size,
+      'size': record,
       'files': {
         'path': metadata.info.files.map(f => f.path),
         'length': metadata.info.files.map(f => f.length)
