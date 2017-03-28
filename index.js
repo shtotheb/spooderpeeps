617ff4f9d3ba3f11c34600c9caa8a6e3fccc26ad
Salim <salimhbalfas@gmail.com>
2017-03-28 13:46:08 +0700

    Another fix

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ p2p.on('metadata', function (metadata) {
   var exists = true;
   Records.findById(metadata.infohash, function(err, doc){
     if (err) console.log(err);
+    console.log("doc =  ", doc);
     if (doc ==  null) exists=false;
   })
   console.log("Metadata found!!  ", metadata.info.name.toString(), " exists = ", exists);
