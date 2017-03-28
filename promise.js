cb0976f5cb1f27d193069e2346d132b807df85f4
Salim <salimhbalfas@gmail.com>
2017-03-28 16:10:48 +0700

    Attempting to use promise

--- promise.js
+++ promise.js
@@ -1,26 +1,46 @@
-watcher.on("add", (fsfile) => {
+watcher.on("add", (metadata) => {
   return new Promise((resolve, reject) => {
-    rt(fsfile, (err, ftorrent) => {
-      if(err) {
+    Records.findById(metadata.infohash, function(err, doc){
+      if (err) {
         reject(err)
       }
-      resolve(ftorrent);
+      if (doc ===  null) {
+        exists=false;
+        resolve(metadata);
+      };
     })
   })
-  .then(ftorrent => [ftorrent, Torrent.findById(ftorrent.infoHash).exec()])
-  .spread((ftorrent, res) => (res) ? Promise.reject("TEXISTS") : Promise.resolve(ftorrent))
-  .then(ftorrent => {
-    return [ftorrent, new Torrent({
-      '_id': ftorrent.infoHash,
-      'title': ftorrent.name,
-      'details': ftorrent.announce,
-      'size': ftorrent.length,
-      'files': ftorrent.files.map(f => f.path),
-      'imported': new Date()
-    }).save()]
+  .then(metadata => {
+    if(typeof metadata.info.name !== 'undefined' && typeof metadata.info.files !== 'undefined' && exists === false){
+      var tempSearch = metadata.info.name.toString();
+      var record = 0;
+
+  		if(typeof metadata.info.files !== 'undefined' && metadata.info.files.length < 100){
+  			var record = [];
+  			metadata.info.files.forEach(function(element){
+            record = record + element.length;
+  			});
+  		}
+
+      var newRecord = new Records({
+        '_id': metadata.infohash,
+        'name': metadata.info.name.toString(),
+        'search': tempSearch.replace(/\.|\_/g, ' '),
+        'magnet': metadata.magnet,
+        'size': record,
+        'files': {
+          'path': metadata.info.files.map(f => f.path),
+          'length': metadata.info.files.map(f => f.length)
+        },
+        'imported': new Date(),
+        'updated': new Date()
+      });
+
+      newRecord.save(function (err, newRecord) {
+        if (err) return console.error(err);
+        console.log(newRecord.name, " metadata saved!")
+      });
+
+  	}
   })
-  .spread((ftorrent, res) => Promise.resolve(logger.info(`File ${ftorrent.infoHash} added ${count+=1}`)))
-  .catch(err => (err==="TEXISTS") ? Promise.resolve(logger.info(`File ${fsfile} already loaded`)) : Promise.reject(err))
-  .then(() => fs.unlinkAsync(fsfile))
-  .catch(err => Promise.reject(logger.error(err)))
 });
